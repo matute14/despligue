eb05c4297608f18acc35e6bbf418231aa9b04b48
matute <matute@matute.local>
2023-03-02 09:52:20 +0000

    Nueva version

---
+++ Nombrejava60.java
@@ -0,0 +1,8 @@
+public class Nombrejava60 {
+
+	public static void main(String[] args) {
+		System.out.println("Me llamo javier matute y he creado el servidor ubuntuserver60 y estoy en San fernando de heanres");
+
+	}
+
+}
